fa35d9dac75b927c69f1b6f53031f185334ba8ea
Nikolay Dagil <Nikolay.Dagil@gmail.com>
2012-09-29 10:54:06 +0300

    Создал метод перевода температуры

--- src/org/Nikolay/main/Main.java
+++ src/org/Nikolay/main/Main.java
@@ -1,5 +1,14 @@
 package org.Nikolay.main;
 
+import org.Nikolay.metod.Metod;
+
 public class Main {
 
+	public static void main(String[] args) {
+
+		Metod metod = new Metod();
+
+		System.out.print(metod.convertTemper(20, "K", "C"));
+
+	}
 }
--- src/org/Nikolay/metod/Metod.java
+++ src/org/Nikolay/metod/Metod.java
@@ -2,4 +2,56 @@ package org.Nikolay.metod;
 
 public class Metod {
 
+	public static double gradus(double a, String x) {
+
+		double temp = 0;
+
+		if (x.equals("k")) {
+			temp = a + 273;
+		} else if (x.equals("f")) {
+			temp = a * 1.8 + 32;
+		}
+
+		return temp;
+	}
+
+	public double convertTemper(double a, String x, String y) {
+
+		double temp = 0;
+
+		switch (x) {
+		case "K":
+
+			switch (y) {
+			case "F":
+				temp = a * 1.8 - 460;
+				break;
+			case "C":
+				temp = a - 273;
+				break;
+			}
+			return temp;
+
+		case "F":
+
+			switch (y) {
+			case "C":
+				temp = (a - 32) * (5 / 9);
+			case "K":
+				temp = (a + 460) * (5 / 9);
+			}
+			return temp;
+
+		case "C":
+
+			switch (y) {
+			case "F":
+				temp = a * 1.8 + 32;
+			case "K":
+				temp = a + 273;
+			}
+			return temp;
+		}
+		return temp;
+	}
 }
